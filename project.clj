2cc1704d6eb3c0275b1923c88b255e7686dcfe8a
Seonho Kim <ksseono@gmail.com>
2017-10-07 17:52:01 +0900

    update dependencies and project info

--- project.clj
+++ project.clj
@@ -1,27 +1,26 @@
-(defproject wit/duckling "0.4.24"
-  :description "Date & Number parser"
-  :license {:url "https://github.com/wit-ai/duckling"
+(defproject ksseono/duckling "0.0.1"
+  :description "The linguist that parses text into structured data, forked from wit-ai/duckling"
+  :license {:url "https://github.com/ksseono/duckling"
             :comments "see LICENSE"}
   :url "https://github.com/wit-ai/duckling"
-  :plugins [[s3-wagon-private "1.1.2"]
-            [lein-midje "3.1.3"]]
+  :plugins [[lein-midje "3.2.1"]]
   :repl-options {:init-ns duckling.core}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.nrepl "0.2.3"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [clj-time "0.8.0"]
-                 [prismatic/plumbing "0.5.3"]]
+                 [org.clojure/tools.nrepl "0.2.12"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [clj-time "0.14.0"]
+                 [prismatic/plumbing "0.5.4"]]
   :deploy-repositories [["clojars" {:creds :gpg}]]
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
-                                  [midje "1.6.3"]
+                                  [midje "1.8.3"]
                                   [cheshire "5.3.1"]]}
              :uberjar {:aot [duckling.core]}}
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
   :scm {:name "git"
-        :url "https://github.com/wit-ai/duckling"}
+        :url "https://github.com/ksseono/duckling"}
   :pom-addition [:developers [:developer
-                               [:name "Wit.ai"]
-                               [:url "https://wit.ai"]
-                               [:email "contact@wit.ai"]
-                               [:timezone "-7"]]])
+                             [:name "Wit.ai"]
+                             [:url "https://github.com/ksseono/duckling"]
+                             [:email "ksseono@gmail.com"]
+                             [:timezone "-7"]]])
